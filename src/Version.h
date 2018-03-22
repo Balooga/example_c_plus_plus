c89a1c2b231d2cf9ff0a29e43dbbe0fab9575410
Luke Crook <luke@balooga.com>
2018-03-22 02:03:27 -0700

    Added Version.h"

---
+++ src/Version.h
@@ -0,0 +1,19 @@
+// -*- mode: c; -*-
+
+#ifndef VERSION_H
+#define VERSION_H
+
+// the configured options and settings for the Example
+#ifndef Example_VERSION_MAJOR
+#define Example_VERSION_MAJOR 0
+#endif
+
+#ifndef Example_VERSION_MINOR
+#define Example_VERSION_MINOR 1
+#endif
+
+#ifndef Example_VERSION_PATCH
+#define Example_VERSION_PATCH 0
+#endif
+
+#endif
